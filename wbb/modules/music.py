857788108f92d78b822f4fe97a51989f712e37eb
TheHamkerCat <TheHamkerCat@gmail.com>
2026-06-29 03:22:03 +0530

    fix: some async fixes

--- wbb/modules/music.py
+++ wbb/modules/music.py
@@ -28,9 +28,9 @@ from asyncio import get_running_loop
 from functools import partial
 from io import BytesIO
 
+import httpx
 from pyrogram import filters
 from pytube import YouTube
-from requests import get
 
 from wbb import aiohttpsession as session
 from wbb import app, arq
@@ -54,14 +54,12 @@ def download_youtube_audio(arq_resp):
     performer = r.channel
 
     m, s = r.duration.split(":")
-    duration = int(
-        datetime.timedelta(minutes=int(m), seconds=int(s)).total_seconds()
-    )
+    duration = int(datetime.timedelta(minutes=int(m), seconds=int(s)).total_seconds())
 
     if duration > 1800:
         return
 
-    thumb = get(r.thumbnails[0]).content
+    thumb = httpx.get(r.thumbnails[0]).content
     with open("thumbnail.png", "wb") as f:
         f.write(thumb)
     thumbnail_file = "thumbnail.png"
@@ -91,9 +89,7 @@ async def music(_, message):
             "Another download is in progress, try again after sometime."
         )
     is_downloading = True
-    m = await message.reply_text(
-        f"Downloading {url}", disable_web_page_preview=True
-    )
+    m = await message.reply_text(f"Downloading {url}", disable_web_page_preview=True)
     try:
         loop = get_running_loop()
         arq_resp = await arq.youtube(url)
